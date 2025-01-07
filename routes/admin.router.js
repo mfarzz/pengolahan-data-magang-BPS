d333b360ec6ff69236d069f59f121e820fd24b4c
daffaagustians <145314938+daffaagustians@users.noreply.github.com>
2025-01-07 09:57:13 +0700

    hapus user yang telah diterima jadi rejected

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { approveUser, rejectUser, tampilUsers, detailUsers } = require('../controllers/admin.controller');
+const { approveUser, rejectUser, tampilUsers, detailUsers, tampilPendaftar, hapusUser } = require('../controllers/admin.controller');
 const { auth } = require('../middlewares/auth.middleware');
 
 router.put('/approve/:userId', auth('admin'), approveUser);
 router.put('/reject/:userId', auth('admin'), rejectUser);
 router.get('/list-users', auth('admin'), tampilUsers);
+router.delete('/list-hapus/:userId', auth('admin'), hapusUser);
+router.get('/list-pendaftar', auth('admin'), tampilPendaftar);
 router.get('/detail-users/:userId', auth('admin'), detailUsers);
 
 module.exports = router;
